2e5473c4695e556b427fcd95ea5e0a804192df6a
cloudcoke <lms4296@gmail.com>
2022-12-11 23:52:08 +0900

    fix: request_parse.js queryString input 타입 수정

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const net = require("net");
+const request = require("./lib/request_parse");
 
 const PORT = process.env.SERVER_PORT || 3000;
 const HOST = process.env.SERVER_HOST || "0.0.0.0";
 
 const server = net.createServer((c) => {
   c.setEncoding("utf-8");
+  c.on("data", (chunk) => {
+    const req = request(chunk);
+    console.log(req);
+  });
 });
 
 server.listen(PORT, HOST, () => {
